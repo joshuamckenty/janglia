32999f3f94387b073599ef8b5bbd5e33ff527b93
Joshua McKenty <jmckenty@joshdev.(none)>
2014-03-10 14:23:28 -0700

    Committing binaries of ganglia bits built as of Iocane for 3.0GA

--- setup.py
+++ setup.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python
 
 import setuptools
+import os
+from setuptools.command.install import install as setuptools_install
+
+GANGLIA_FILES = [
+    ('/usr/local/sbin', ['files/gmond']),
+    ('/usr/local/bin', ['files/ganglia-config', 'files/gmetric', 'files/gstat']),
+    ('/usr/local/lib', ['files/libconfuse.a', 'files/libconfuse.la', 'files/libconfuse.so.0.0.0']),
+    ('/usr/local/lib/pkgconfig', ['files/libconfuse.pc']),
+    ('/usr/local/lib64', ['files/libganglia.a', 'files/libganglia.la', 
+        'files/libganglia-3.6.0.so.0.0.0']),
+    ('/usr/local/lib64/ganglia', ['files/modcpu.so', 'files/moddisk.so', 
+        'files/modload.so', 'files/modmem.so',
+        'files/modmulticpu.so', 'files/modnet.so', 'files/modproc.so', 
+        'files/modpython.so', 'files/modsys.so']),
+    ('/usr/local/etc/conf.d', ['files/modpython.conf'])
+] 
+
+GANGLIA_SYMLINKS = [
+    ('/usr/local/lib/libconfuse.so.0.0.0', '/usr/local/lib/libconfuse.so'),
+    ('/usr/local/lib/libconfuse.so.0.0.0', '/usr/local/lib/libconfuse.so.0'),
+    ('/usr/local/lib64/libganglia-3.6.0.so.0.0.0', 'libganglia-3.6.0.so.0'),
+    ('/usr/local/lib64/libganglia-3.6.0.so.0.0.0', 'libganglia.so'),
+]
+
+class install(setuptools_install):
+    def run(self):
+        super(install, self).run()
+        # Data files don't get their execute bit set, so fix that.
+        for dirname, files in GANGLIA_FILES:
+            if dirname in ['/usr/local/sbin', '/usr/local/bin']:
+                for filename in files:
+                    os.chmod(os.path.join(dirname, os.path.basename(filename)), 0755)
+        for src, target in GANGLIA_SYMLINKS:
+            os.symlink(src, target)
+
 
 setuptools.setup(
     name='janglia',
@@ -20,5 +55,7 @@ setuptools.setup(
         'piston.moxie.v1.configuration': [
             'GangliaTask = janglia:GangliaTask',
         ]
-    }
+    },
+    data_files=GANGLIA_FILES,
+    cmdclass={'install': install}
 )
